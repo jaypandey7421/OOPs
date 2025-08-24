7f578dadb7a81c35ff3b61732fa20b4b23cea953
Jay Pandey <jaypandey7421@gmail.com>
2025-08-24 23:01:09 +0530

    Menu Driven

--- BankAccountManagementSystem/main.cpp
+++ BankAccountManagementSystem/main.cpp
@@ -1,26 +1,29 @@
 #include <iostream>
 #include <string>
 #include <utility>
+#include <vector>
+#include <limits>
+
 using namespace std;
 
 class Account{
 private:
     long long accountNumber;
     string name;
-    int balance;
+    double balance;
 public:
-    Account(const long long a, string n, const int b): accountNumber(a), name(std::move(n)), balance(b){}
+    Account(const long long a, string n, const double b): accountNumber(a), name(std::move(n)), balance(b){}
 
-    void add(const int b){
+    void deposit(const double b){
         if(b < 1){
             cout<<"Add valid amount!"<<endl;
             return;
         }
-
         balance += b;
+        cout<<"Total balance: "<<balance<<endl;
     }
 
-    void withdraw(const int amount){
+    void withdraw(const double amount){
         if(amount > balance){
             cout<<"Not sufficient balance!"<<endl;
             return;
@@ -30,6 +33,11 @@ public:
         }
 
         balance -= amount;
+        cout<<"Remaining balance: "<<balance<<endl;
+    }
+
+    [[nodiscard]] long long getAccountNumber() const{
+        return accountNumber;
     }
 
     void accountInfo() const{
@@ -41,27 +49,125 @@ public:
         cout<<"|_____________________________________________"<<endl;
     }
 
-    [[nodiscard]] int checkBalance() const{
+    [[nodiscard]] double checkBalance() const{
         return balance;
     }
 
 };
 
-int main(){
-    Account a(1113912710003, "Batman of Gotham", 100);
+Account* findAccount(vector<Account>& accounts, long long accountNumber) {
+    for (auto& account : accounts) {
+        if (account.getAccountNumber() == accountNumber) {
+            return &account;
+        }
+    }
+    return nullptr;
+}
 
-    a.accountInfo();
-    cout<<"Balance: $"<<a.checkBalance()<<endl;
-    a.add(50);
-    cout<<"Balance: $"<<a.checkBalance()<<endl;
-    a.withdraw(-20);
-    cout<<"Balance: $"<<a.checkBalance()<<endl;
-    a.withdraw(200);
-    cout<<"Balance: $"<<a.checkBalance()<<endl;
-    a.withdraw(20);
-    cout<<"Balance: $"<<a.checkBalance()<<endl;
-    a.add(30);
-    cout<<"Balance: $"<<a.checkBalance()<<endl;
+int main(){
+    vector<Account> accounts;
+
+    int choice = 0;
+    cout<<"1 Create an account."<<endl;
+    cout<<"2 Check balance."<<endl;
+    cout<<"3 Deposit."<<endl;
+    cout<<"4 Withdraw."<<endl;
+    cout<<"5 Exit."<<endl;
+    do {
+        cout<<"Enter your choice: ";
+        cin>>choice;
+        cout<<endl;
+
+        if (cin.fail()) {
+            cout << "Invalid input. Please enter a number." << endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            continue;
+        }
+
+        switch(choice) {
+            case 1: {
+                long long accountNumber;
+                string name;
+                double initialBalance;
+
+                cout<<"Enter your 9 digit account number: ";
+                cin>>accountNumber;
+                cout<<endl<<"Enter your name: ";
+                cin.ignore();
+                getline(cin, name);
+                cout<<"Enter your initial balance: ";
+                cin>>initialBalance;
+                cout<<endl;
+                accounts.emplace_back(accountNumber, name, initialBalance);
+                cout<<"Account created successfully!"<<endl;
+                break;
+            }
+            case 2: {
+                long long accountNumber;
+                const Account* accountDetails = nullptr;
+                cout<<"Enter your 9 digit account number: ";
+                cin>>accountNumber;
+                cout<<endl;
+
+                accountDetails = findAccount(accounts, accountNumber);
+                if(accountDetails != nullptr) {
+                    accountDetails->accountInfo();
+                    break;
+                }else {
+                    cout<<"Account not found."<<endl;
+                    break;
+                }
+            }
+            case 3: {
+                long long accountNumber;
+                Account* accountDetails = nullptr;
+                cout<<"Enter your 9 digit account number: ";
+                cin>>accountNumber;
+                cout<<endl;
+
+                accountDetails = findAccount(accounts, accountNumber);
+                if(accountDetails != nullptr) {
+                    double amount;
+                    cout<<"Enter the amount to deposit: ";
+                    cin>>amount;
+                    accountDetails->deposit(amount);
+                    break;
+                }else {
+                    cout<<"Account not found."<<endl;
+                    break;
+                }
+            }
+            case 4: {
+                long long accountNumber;
+                Account* accountDetails = nullptr;
+                cout<<"Enter your 9 digit account number: ";
+                cin>>accountNumber;
+                cout<<endl;
+
+                accountDetails = findAccount(accounts, accountNumber);
+                if(accountDetails != nullptr) {
+                    double amount;
+                    cout<<"Enter the amount to withdraw: ";
+                    cin>>amount;
+                    accountDetails->withdraw(amount);
+                    break;
+                }else {
+                    cout<<"Account not found."<<endl;
+                    break;
+                }
+            }
+            case 5: {
+                cout<<"Exited Successfully!."<<endl;
+                break;
+            }
+            default: {
+                cout<<"Invalid choice. Try agin!"<<endl;
+            }
+        }
+
+
+    }while (choice != 5);
 
 
     return 0;
